ea96939a7ed1a95e34f8ed91919a74207f18af79
s46129 <s46129@yahoo.com.tw>
2023-07-12 22:01:43 +0800

    feat : Implement API Manager

---
+++ Assets/Scripts/API/APIManager.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class APIManager : MonoBehaviour
+{
+    public IEnumerator Get(string uri, Action<Response> response)
+    {
+        using var request = UnityWebRequest.Get(uri);
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError)
+        {
+            var _response = new Response
+            {
+                isSuccess = false,
+                result = request.error
+            };
+            response?.Invoke(_response);
+            Debug.Log(request.error);
+        }
+        else
+        {
+            var _response = new Response
+            {
+                isSuccess = true,
+                result = request.downloadHandler.text
+            };
+            response?.Invoke(_response);
+            Debug.Log(request.downloadHandler.text);
+        }
+    }
+}
+
+public class Response
+{
+    public bool isSuccess;
+    public string result;
+}
